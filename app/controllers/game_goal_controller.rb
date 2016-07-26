d4d7f228e244a76f1320c7371bc5c13ac4529b49
jd2rogers2 <jd2rogers2@gmail.com>
2016-07-25 20:06:27 -0400

    changed game date addgoals controller from date to datetime

--- app/controllers/game_goal_controller.rb
+++ app/controllers/game_goal_controller.rb
@@ -13,13 +13,12 @@ class GameGoalController < ApplicationController
     if is_logged_in?
       if params[:game_date][:year].to_s.match(/\b\d{4}\b/) && params[:game_date][:month].to_s.match(/\b\d{1,2}\b/) && params[:game_date][:day].to_s.match(/\b\d{1,2}\b/)
         @player = Player.find_by_id(session[:id])
-        Time.new(params[:game_date][:year], params[:game_date][:month], params[:game_date][:day])
-        binding.pry
-        # @game = Game.create(date: params[:game_date].to_i)
-        # @game.teams << Team.find_by(name: params[:home_team])
-        # @game.teams << Team.find_by(name: params[:away_team])
-        # @player.games << @game
-        # erb :'/game_and_goal/add_goals'
+        @time = Time.new(params[:game_date][:year].to_i, params[:game_date][:month].to_i, params[:game_date][:day].to_i)
+        @game = Game.create(datetime: @time)
+        @game.teams << Team.find_by(name: params[:home_team])
+        @game.teams << Team.find_by(name: params[:away_team])
+        @player.games << @game
+        erb :'/game_and_goal/add_goals'
       else
         flash[:message] = "game date must be numerals in dd/mm/yyyy format"
         redirect '/game/new'
@@ -34,6 +33,7 @@ class GameGoalController < ApplicationController
     #currently redirecting to the wrong place and not adding goals
     # added month and day to view and changed params, time to update controller
     # also check first two notes above
+    binding.pry
     if is_logged_in?
       @game = Game.find_by(date: params[:date])
       if params[:home_goal_minutes].to_s.match(/\b\d{0,2}\b/) && params[:away_goal_minutes].to_s.match(/\b\d{0,2}\b/)# two numbers
